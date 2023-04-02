e7393861975bbb72dcb65150685b6b19ed3c56f8
weronikakatarzyna <126699713+weronikakatarzyna@users.noreply.github.com>
2023-04-02 17:42:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
